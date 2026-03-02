bbc15a7687ccd3a8365530d549d3a074c6778bd4
Jonathan Sesma <jonathan.sesma@outlook.com>
2026-03-01 18:51:11 -0600

    team model modified

---
+++ backend/app/models/team.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String
+from app.database import Base
+
+class Team(Base):
+    __tablenae__ = "teams"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    stadium = Column(String)
+    
