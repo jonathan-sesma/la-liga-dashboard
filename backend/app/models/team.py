48a3834d50401ab5b36d76320a86049bbeac8ba6
Jonathan Sesma <jonathan.sesma@outlook.com>
2026-03-01 20:20:52 -0600

    got rid of a typo in teams.py

--- backend/app/models/team.py
+++ backend/app/models/team.py
@@ -2,9 +2,8 @@ from sqlalchemy import Column, Integer, String
 from app.database import Base
 
 class Team(Base):
-    __tablenae__ = "teams"
+    __tablename__ = "teams"
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
     stadium = Column(String)
-    
