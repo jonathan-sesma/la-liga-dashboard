cd1178b8254e43ce6928f9e3ba448f374d71d7bf
Jonathan S. <jonathan.sesma@outlook.com>
2026-03-09 14:31:02 -0600

    m

--- backend/app/services/football_api.py
+++ backend/app/services/football_api.py
@@ -1,5 +1,5 @@
-import requests
+import http.client
 
-API_KEY = e26353a17d32f2366cc2f5e5ab5ea0f8
+API_KEY = "e26353a17d32f2366cc2f5e5ab5ea0f8"
 
-BASE_URL = ""
+BASE_URL = "https://v3.football.api-sports.io/standings"
