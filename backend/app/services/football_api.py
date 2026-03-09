b6e1b8123fb2b4939f29d6b5675f00b5b006b3a3
Jonathan S. <jonathan.sesma@outlook.com>
2026-03-09 14:45:54 -0600

    m

--- backend/app/services/football_api.py
+++ backend/app/services/football_api.py
@@ -1,5 +1,17 @@
-import http.client
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from app.database import get_db
 
 API_KEY = "e26353a17d32f2366cc2f5e5ab5ea0f8"
 
-BASE_URL = "https://v3.football.api-sports.io/standings"
+BASE_URL = "https://v3.football.api-sports.io"
+
+headers = {
+    'x-apisports-key': API_KEY
+}
+
+router = APIRouter(tags=["Standings"])
+
+@router.get("/")
+def get_standings(db: Session, Depends:(get_db)):
+
