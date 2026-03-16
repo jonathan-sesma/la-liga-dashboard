819a7ee19de54e08356e2026b6dbc520bd5dad84
Jonathan Sesma <jonathan.sesma@outlook.com>
2026-03-16 00:54:52 -0600

    m

---
+++ backend/app/services/scheduler.py
@@ -0,0 +1,17 @@
+from app.database import SessionLocal
+from app.services.football_api import get_and_sync_teams
+import logging
+
+logger = logging.getLogger(__name__)
+
+async def sync_la_liga_data():
+    logger.info("Scheduled Sync Started: Updating La Liga Teams...")
+
+    db = SessionLocal()
+    try:
+        await get_and_sync_teams(db=db, league_id=140)
+        logger.info("Scheduled sync completed successfully.")
+    except Exception as e:
+        logger.error(f"Sync failed: {e}")
+    finally:
+        db.close()
