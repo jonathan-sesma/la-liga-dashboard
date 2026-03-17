5b1391ed89111cac81dd9cfa7155005d102b3c7d
Jonathan Sesma <jonathan.sesma@outlook.com>
2026-03-16 18:48:52 -0600

    m

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,6 +8,7 @@ import logging
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s  - %(levelname)s - %(message)s')
+
 logger = logging.getLogger(__name__)
 
 @asynccontextmanager
