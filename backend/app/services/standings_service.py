003bf2a53d96bdda2cf5a96de7d20bb91f442fcc
Jonathan Sesma <jonathan.sesma@outlook.com>
2026-05-10 20:50:38 -0600

    m

--- backend/app/services/standings_service.py
+++ backend/app/services/standings_service.py
@@ -7,120 +7,42 @@ from app.config import settings
 from app.models.standing import Standing
 
 logger = logging.getLogger(__name__)
+
 async def get_and_sync_standings(db: Session, league_id: int):
-    existing_stands = db.query(Standing).filter(
-        Standing.league_id == league_id).all()
-    
+
+    existing = get_standings(db, league_id)
+
     if existing:
-        return existing_stands
+         return existing
 
     headers = {'x-apisports-key': settings.FOOTBALL_API_KEY}
     url = f"{settings.FOOTBALL_API_URL}/standings?league={league_id}&season=2024"
 
-    async with httpx.AsyncClient() as client:
-        try:
-            response = await client.get(url, headers=headers)
-            response.raise_for_status()
+    data = await fetch_standings_from_api(url, headers, db)
 
-            data = response.json()["response"][0]["league"]["standings"][0]
+    await upsert_standings(db, data, league_id)
 
-            for item in data:
-                team_id = item["team"]["id"]
-
-                existing = db.query(Standing).filter(
-                    Standing.team_id == team_id,
-                    Standing.league_id == league_id
-                ).first()
-
-                if existing:
-                    existing.position = item["rank"]
-                    existing.points = item["points"]
-                    existing.played = item["all"]["played"]
-                    existing.wins = item["all"]["win"]
-                    existing.losses = item["all"]["lose"]
-                    existing.draws = item["all"]["draw"]
-                    existing.goals_for = item["all"]["goals"]["for"]
-                    existing.goals_against = item["all"]["goals"]["against"]
-                    existing.goal_difference = item["goalsDiff"]
-                    
-                else:
-                    new_standing = Standing(
-                    team_id=team_id,
-                    league_id=league_id,
-                    position=item["rank"],
-                    points=item["points"],
-                    played=item["all"]["played"],
-                    wins=item["all"]["win"],
-                    losses=item["all"]["lose"],
-                    draws=item["all"]["draw"],
-                    goals_for=item["all"]["goals"]["for"],
-                    goals_against=item["all"]["goals"]["against"],
-                    goal_difference=item["goalsDiff"]
-                    )
-
-                    db.add(new_standing)
-
-            db.commit()
-            return db.query(Standing).filter(Standing.league_id == league_id).all()
-
-        except httpx.HTTPStatusError as exc:
-            db.rollback()
-            raise HTTPException(status_code=exc.response.status_code,detail="Error fetching standings from API-FOOTBALL")
-        
-        except Exception as exc:
-            db.rollback()
-            raise HTTPException(
-                status_code=500,
-                detail=f"Unexpected error: {str(exc)}"
-            )
+    return get_standings(db, league_id)
 
 async def sync_standings(db: Session, league_id: int):
     headers = {'x-apisports-key': settings.FOOTBALL_API_KEY}
     url = f"{settings.FOOTBALL_API_URL}/standings?league={league_id}&season=2024"
 
+    data = await fetch_standings_from_api(url, headers, db)
+
+    upsert_standings(db, data, league_id)
+
+    return get_standings(db, league_id)
+
+        
+async def fetch_standings_from_api(url, headers, db):
+
     async with httpx.AsyncClient() as client:
         try:
             response = await client.get(url, headers=headers)
             response.raise_for_status()
             data = response.json()["response"][0]["league"]["standings"][0]
-
-            logger.info(f"Api Response {json.dumps(data, indent=2)}")
-
-            for item in data:
-                existing = db.query(Standing).filter(
-                    Standing.team_id == item["team"]["id"],
-                    Standing.league_id == league_id
-                ).first()
-
-                if existing:
-                    existing.position = item["rank"]
-                    existing.points = item["points"]
-                    existing.played = item["all"]["played"]
-                    existing.wins = item["all"]["win"]
-                    existing.losses = item["all"]["lose"]
-                    existing.draws = item["all"]["draw"]
-                    existing.goals_for = item["all"]["goals"]["for"]
-                    existing.goals_against = item["all"]["goals"]["against"]
-                    existing.goal_difference = item["goalsDiff"]
-
-                else:
-                    new_standing = Standing(
-                    team_id=item["team"]["id"],
-                    league_id=league_id,
-                    position=item["rank"],
-                    points=item["points"],
-                    played=item["all"]["played"],
-                    wins=item["all"]["win"],
-                    losses=item["all"]["lose"],
-                    draws=item["all"]["draw"],
-                    goals_for=item["all"]["goals"]["for"],
-                    goals_against=item["all"]["goals"]["against"],
-                    goal_difference=item["goalsDiff"]
-                    )
-
-                    db.add(new_standing)
-
-            db.commit()
+            return data
         
         except httpx.HTTPStatusError as exc:
             db.rollback()
@@ -128,10 +50,56 @@ async def sync_standings(db: Session, league_id: int):
                 status_code=exc.response.status_code,
                 detail="Error fetching standings from API-FOOTBALL"
             )
-        
+
         except Exception as exc:
             db.rollback()
             raise HTTPException(
                 status_code=500,
                 detail=f"Unexpected error: {str(exc)}"
             )
+
+
+def upsert_standings(db: Session, data, league_id):
+    for item in data:
+        existing = db.query(Standing).filter(
+            Standing.team_id == item["team"]["id"],
+            Standing.league_id == league_id
+        ).first()
+
+        if existing:
+            existing.position = item["rank"]
+            existing.points = item["points"]
+            existing.played = item["all"]["played"]
+            existing.wins = item["all"]["win"]
+            existing.losses = item["all"]["lose"]
+            existing.draws = item["all"]["draw"]
+            existing.goals_for = item["all"]["goals"]["for"]
+            existing.goals_against = item["all"]["goals"]["against"]
+            existing.goal_difference = item["goalsDiff"]
+
+        else:
+            new_standing = Standing(
+            team_id=item["team"]["id"],
+            league_id=league_id,
+            position=item["rank"],
+            points=item["points"],
+            played=item["all"]["played"],
+            wins=item["all"]["win"],
+            losses=item["all"]["lose"],
+            draws=item["all"]["draw"],
+            goals_for=item["all"]["goals"]["for"],
+            goals_against=item["all"]["goals"]["against"],
+            goal_difference=item["goalsDiff"]
+            )
+
+            db.add(new_standing)
+
+    db.commit()
+
+
+def get_standings(db: Session, league_id):
+    existing_stands = db.query(Standing).filter(
+        Standing.league_id == league_id).all()
+    
+    if existing_stands:
+        return existing_stands
